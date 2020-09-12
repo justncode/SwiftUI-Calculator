8d567230d0b15f6ffef2938e309a6ed1524b5d38
Juke Jaster <justin@justncode.com>
2020-09-12 11:32:43 -0400

    Handle orientation changes for landscape mode.

--- SwiftUICalculator/ContentView.swift
+++ SwiftUICalculator/ContentView.swift
@@ -1,13 +1,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    
     @State private var result = "0"
     
     var body: some View {
         GeometryReader { geometry in
-            VStack(spacing: 0) {
-                ResultView(result: self.$result, geometry: geometry)
-                EntryView(result: self.$result)
+            Group {
+                if self.verticalSizeClass == .regular {
+                    VStack(spacing: 0) {
+                        ResultView(result: self.$result, geometry: geometry)
+                        EntryView(result: self.$result)
+                    }
+                } else {
+                    HStack(spacing: 0) {
+                        ResultView(result: self.$result, geometry: geometry)
+                        EntryView(result: self.$result)
+                    }
+                }
             }
             .preferredColorScheme(.dark)
             .background(Color.black.edgesIgnoringSafeArea(.all))
